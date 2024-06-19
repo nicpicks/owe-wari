f9eee35a44df3451268c00cb64063e9e67d44bfa
nicpicks <wychia21@gmail.com>
2024-06-20 00:40:32 +0800

    feat: hero page and create group form

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,33 @@ export default {
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
       },
+      textColor: {
+        DEFAULT: '#000'
+      },
+      backgroundColor: {
+        DEFAULT: '#FFF'
+      }
     },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#9796f0",
+          "primary-darker": "#7c7ad6",
+          "secondary": "#00be3e",
+          "accent": "#5d00ff",
+          "neutral": "#00070c",
+          "base-100": "#fffbf5",
+          "info": "#00beff",
+          "success": "#00d25d",
+          "warning": "#ff9700",
+          "error": "#ff164d",
+        },
+      },
+    ]
+  }
 } satisfies Config;
